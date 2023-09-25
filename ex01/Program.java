83d89eba586912ad86994f4e2944f189d8209c58
GOAT095 <hakimoxxx@gmail.com>
2023-09-25 15:34:54 +0100

    ex01- small fix to counter + 1

--- ex01/Program.java
+++ ex01/Program.java
@@ -6,22 +6,22 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
         if(n >= 0 && n <= 1) {
             System.err.println("IllegalArgument");
+            scanner.close();
             System.exit(-1); //error
         }
         int i = 2;
         int counter = 0;
-        while (i * i < n) {
+        while (i * i <= n) {
             counter++;
             if (n % i == 0) {
                 System.out.println("false " + counter);
-                break;
+                return;
             }
             i++;
         }
-        System.out.println("true " + counter + 1);
+        System.out.println("true " + (counter + 1));
         scanner.close();
         }
 }
