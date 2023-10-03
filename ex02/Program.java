b799f2c585e69b22ef5bc4de6e60eb2c51db68ba
GOAT095 <hakimoxxx@gmail.com>
2023-10-03 11:35:12 +0100

    ex02 wrong res

---
+++ ex02/Program.java
@@ -0,0 +1,34 @@
+
+
+package ex02;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in); //get input from stdin in java
+        int i = 2;
+        int counter= 0;
+        int res = 0;
+        int n;
+        while((n = scanner.nextInt()) != 42){
+            while(n != 0) {
+                res += n % 10;
+                n /= 10;
+            }
+            while (i * i <= res) {
+                if (res % i == 0) {
+                    System.out.println("---> " + res);
+                    counter--;
+                    break;
+                }
+                i++;
+            }
+            counter++;
+            System.out.println("---> " + counter);
+        }
+        System.out.println("Count of coffee-request - " + counter);
+        scanner.close();
+    }
+}
+
