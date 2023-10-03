b7723a96e61605457ba5a4b933568124b9a6ae38
GOAT095 <hakimoxxx@gmail.com>
2023-10-03 12:34:12 +0100

    ex02 works fine

--- ex02/Program.java
+++ ex02/Program.java
@@ -7,16 +7,18 @@ import java.util.Scanner;
 public class Program {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in); //get input from stdin in java
-        int i = 2;
+        int i;
         int counter= 0;
         int res = 0;
         int n;
         while((n = scanner.nextInt()) != 42){
+            i = 2;
             while(n != 0) {
                 res += n % 10;
                 n /= 10;
             }
             while (i * i <= res) {
+                System.out.println("---> " + res);
                 if (res % i == 0) {
                     System.out.println("---> " + res);
                     counter--;
