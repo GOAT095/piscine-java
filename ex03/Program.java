750c8f228469fb2fc2af90fdb6585398d74d3b48
GOAT095 <hakimoxxx@gmail.com>
2023-11-09 17:32:57 +0100

    3

--- ex03/Program.java
+++ ex03/Program.java
@@ -11,27 +11,45 @@ public class Program {
         }
         return min;
     }
-    private static void print_grades(){
-
-    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s ;
         int CurrentWeek = 1;
+        int res = 0;
+
+        int multiply = 1;
         while (1 == 1){ // autism
             if((s = scanner.next()).equals("42") || CurrentWeek > 18)
                 break;
-            if(s.equals("Week")){
-                int Wentered = scanner.nextInt();
-                if(Wentered != CurrentWeek){
+            if(s.equals("Week")) {
+                int WeekEntered = scanner.nextInt();
+                if (WeekEntered != CurrentWeek) {
                     System.err.println("IllegalArgument");
                     scanner.close();
                     System.exit(-1);
                 }
                 int min = get_min(scanner);
                 System.out.println(min);
-
+                int pow = CurrentWeek - 1;
+                while (pow > 0) {
+                    multiply *= 10;
+                    pow--;
+                }
+                System.out.println("multi = " + multiply);
+                res += multiply * min;
+                System.out.println("result = " + res);
+                CurrentWeek++;
             }
         }
+        // printer
+        for (int i = 0; i < CurrentWeek-1; i++) {
+            System.out.print("Week " + (i + 1) + " ");
+            for (int j = 0; j < res % 10; j++) {
+                System.out.print("=");
+            }
+            System.out.println(">");
+            res = res / 10;
+        }
+        scanner.close();
     }
 }
